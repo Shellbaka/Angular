6efdb12b56e061a78edd8dbb180e7716344bc5cd
Shellbaka <shellbaka69@gmail.com>
2025-03-05 14:09:34 -0300

    Alterações feitas acrescentando novas páginas e adciononando os elementos de novos componentes, como o Header, os Avisos da tela, o Header 2 só para os botões e afins. Também estou mexendo com os css mas vou dar um reset agora para ver pq tem alguns elementos sem funcionar as características.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { Header2Component } from './header-2/header-2.component'; 
+import { AvisosComponent } from './avisos/avisos.component';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: 'app-root', 
+  templateUrl: './app.component.html', 
+  styleUrls: ['./app.component.css'],  
+  standalone: true,  
+  imports: [HeaderComponent, Header2Component, AvisosComponent]  
 })
 export class AppComponent {
-  title = 'Angular';
 }
